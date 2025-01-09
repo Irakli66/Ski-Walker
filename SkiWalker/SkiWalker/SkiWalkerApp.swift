17cfbd454b8a4b088458704b67da27fe741b66be
Irakli <99668662+Irakli66@users.noreply.github.com>
2025-01-09 20:22:19 +0400

    inittial commit

---
+++ SkiWalker/SkiWalker/SkiWalkerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkiWalkerApp.swift
+//  SkiWalker
+//
+//  Created by irakli kharshiladze on 09.01.25.
+//
+
+import SwiftUI
+
+@main
+struct SkiWalkerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
