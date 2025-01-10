0d203768b578066ed84716f2c689a7896690e025
Irakli <99668662+Irakli66@users.noreply.github.com>
2025-01-10 15:45:17 +0400

    add: custom reusable button

---
+++ SkiWalker/SkiWalker/View/Components/CustomButton.swift
@@ -0,0 +1,29 @@
+//
+//  CustomButton.swift
+//  SkiWalker
+//
+//  Created by irakli kharshiladze on 10.01.25.
+//
+import UIKit
+
+final class CustomButton: UIButton {
+    
+    init(buttonText: String) {
+        super.init(frame: .zero)
+        setupButton(buttonText: buttonText)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupButton(buttonText: String) {
+        self.translatesAutoresizingMaskIntoConstraints = false
+        self.backgroundColor = .customPurple
+        self.setTitle(buttonText, for: .normal)
+        self.setTitleColor(.customWhite, for: .normal)
+        self.layer.cornerRadius = 10
+        self.clipsToBounds = true
+        self.heightAnchor.constraint(equalToConstant: 50).isActive = true
+    }
+}
