0bc3eec1139f24c7db629f2409e22b320710170b
Irakli <99668662+Irakli66@users.noreply.github.com>
2025-01-12 21:36:34 +0400

    update:order cell ui for multiple products

--- SkiWalker/SkiWalker/View/OrderHistory/OrderHistoryCellView.swift
+++ SkiWalker/SkiWalker/View/OrderHistory/OrderHistoryCellView.swift
@@ -11,18 +11,33 @@ struct OrderHistoryCellView: View {
     
     var body: some View {
         HStack(spacing: 12) {
-            Image(systemName: order.products[0].imageURLs[0])
-                .resizable()
-                .scaledToFill()
-                .frame(width: 100)
-                .background(Color.gray.opacity(0.1))
-                .clipShape(RoundedRectangle(cornerRadius: 10))
-                .shadow(color: .black.opacity(0.1), radius: 4, x: 0, y: 2)
+            ZStack(alignment: .center) {
+                Image(systemName: order.products[0].imageURLs[0])
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 100, height: 100)
+                    .background(Color.gray.opacity(0.1))
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                    .shadow(color: .black.opacity(0.1), radius: 4, x: 0, y: 2)
+                    .clipped()
+                
+                if order.products.count > 1 {
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(Color.black.opacity(0.4))
+                        .frame(width: 100, height: 100)
+                    
+                    Text("+\(order.products.count - 1)")
+                        .font(.headline)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color.white)
+                    
+                }
+            }
             
             VStack(alignment: .leading, spacing: 8) {
                 HStack(spacing: 5) {
                     Image(systemName: "doc.plaintext")
-                        .foregroundColor(.customPurple)
+                        .foregroundColor(Color.customPurple)
                     Text("Order:")
                         .font(.subheadline)
                         .foregroundColor(.gray)
@@ -33,7 +48,7 @@ struct OrderHistoryCellView: View {
                 
                 HStack(spacing: 5) {
                     Image(systemName: "calendar")
-                        .foregroundColor(.customPurple)
+                        .foregroundColor(Color.customPurple)
                     Text("Date:")
                         .font(.subheadline)
                         .foregroundColor(.gray)
@@ -58,14 +73,14 @@ struct OrderHistoryCellView: View {
                 
                 HStack(spacing: 5) {
                     Image(systemName: "dollarsign.circle")
-                        .foregroundColor(.customPurple)
+                        .foregroundColor(Color.customPurple)
                     Text("Total Price:")
                         .font(.subheadline)
                         .foregroundColor(.gray)
                     Text("$\(order.totalPrice, specifier: "%.2f")")
                         .font(.headline)
                         .fontWeight(.bold)
-                        .foregroundColor(.customPurple)
+                        .foregroundColor(Color.customPurple)
                 }
             }
             
