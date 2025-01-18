f85c69593603bfcf7cf152112b26ba953a987ab7
Irakli <99668662+Irakli66@users.noreply.github.com>
2025-01-18 20:21:45 +0400

    add: reusable stepper view

--- SkiWalker/SkiWalker/View/Components/ReusableStepperView.swift
+++ SkiWalker/SkiWalker/View/Components/ReusableStepperView.swift
@@ -8,11 +8,37 @@
 import SwiftUI
 
 struct ReusableStepperView: View {
+    let stock: Int
+    @Binding var quantity: Int
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            Button(action: {
+                if quantity > 1 {
+                    quantity -= 1
+                }
+            }) {
+                Image(systemName: "minus.circle.fill")
+                    .font(.title2)
+                    .foregroundColor(quantity > 1 ? Color.customPurple : Color.gray)
+            }
+            
+            Text("\(quantity)")
+                .font(.headline)
+                .frame(minWidth: 40)
+
+            Button(action: {
+                if quantity < stock {
+                    quantity += 1
+                }
+            }) {
+                Image(systemName: "plus.circle.fill")
+                    .font(.title2)
+                    .foregroundColor(quantity < stock ? Color.customPurple : Color.gray)
+            }
+        }
+        .padding(8)
+        .background(Color.customPurple.opacity(0.1))
+        .cornerRadius(10)
     }
 }
 
-#Preview {
-    ReusableStepperView()
-}
