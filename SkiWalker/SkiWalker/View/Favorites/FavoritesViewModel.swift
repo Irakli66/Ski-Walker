76abc97bd81ac2488ff05952c2a042652249ee1a
Irakli <99668662+Irakli66@users.noreply.github.com>
2025-01-21 17:02:16 +0400

    add: favorites view model

---
+++ SkiWalker/SkiWalker/View/Favorites/FavoritesViewModel.swift
@@ -0,0 +1,53 @@
+//
+//  FavoritesViewModel.swift
+//  SkiWalker
+//
+//  Created by irakli kharshiladze on 21.01.25.
+//
+import Foundation
+
+final class FavoritesViewModel {
+    private let favoritesManager: FavoritesManagerProtocol
+    private let cartManager: CartManagerProtocol
+    private var favorites: [Product] = []
+    
+    init(favoritesManager: FavoritesManagerProtocol = FAvoritesManager(), cartManager: CartManagerProtocol = CartManager()) {
+        self.favoritesManager = favoritesManager
+        self.cartManager = cartManager
+    }
+    
+    func fetchFavorites() async {
+        do {
+            let response: [Product] = try await favoritesManager.fetchFavorites()
+            await MainActor.run {
+                favorites = response
+            }
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    func addProductToCart(productId: String, count: Int = 1) async throws {
+        do {
+            try await cartManager.addProductToCart(productId: productId, count: count)
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    func deleFavorite(with id: String) async  {
+        do {
+            try await favoritesManager.deleteFromFavorites(with: id)
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    func getFavoritesCount() -> Int {
+        favorites.count
+    }
+    
+    func getFavorite(at index: Int) -> Product {
+        return favorites[index]
+    }
+}
