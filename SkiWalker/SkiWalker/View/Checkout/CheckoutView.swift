b12c5c2be2a22a8cd77002b806a56424345e8717
Irakli <99668662+Irakli66@users.noreply.github.com>
2025-01-22 18:19:09 +0400

    add: navigation to checkout from cart

---
+++ SkiWalker/SkiWalker/View/Checkout/CheckoutView.swift
@@ -0,0 +1,18 @@
+//
+//  CheckoutView.swift
+//  SkiWalker
+//
+//  Created by irakli kharshiladze on 22.01.25.
+//
+
+import SwiftUI
+
+struct CheckoutView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CheckoutView()
+}
