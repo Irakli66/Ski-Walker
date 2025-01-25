f455a2d45d66abab1e175c8b5b224c5024c4fc12
Irakli <99668662+Irakli66@users.noreply.github.com>
2025-01-25 22:32:24 +0400

    add: browsing history item model

---
+++ SkiWalker/SkiWalker/View/Home/SubCategoryView/BrowsingHistory/BrowsingHistoryView.swift
@@ -0,0 +1,18 @@
+//
+//  BrowsingHistoryView.swift
+//  SkiWalker
+//
+//  Created by irakli kharshiladze on 25.01.25.
+//
+
+import SwiftUI
+
+struct BrowsingHistoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BrowsingHistoryView()
+}
