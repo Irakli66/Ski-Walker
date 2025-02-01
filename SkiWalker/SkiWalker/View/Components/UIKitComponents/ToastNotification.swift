f60b41f96bad05bbe7f91abf2fb0dc10f6bf370c
Irakli <99668662+Irakli66@users.noreply.github.com>
2025-02-01 18:39:33 +0400

    update: folder architecture

--- SkiWalker/SkiWalker/View/Components/UIKitComponents/ToastNotification.swift
+++ SkiWalker/SkiWalker/View/Components/UIKitComponents/ToastNotification.swift
@@ -4,7 +4,7 @@
 //
 //  Created by irakli kharshiladze on 15.01.25.
 //
-import SwiftUI
+import UIKit
 
 enum ToastType {
     case success
@@ -29,7 +29,6 @@ enum ToastType {
     }
 }
 
-//UIKit Toast
 final class ToastView: UIView {
     
     private let messageLabel = UILabel()
@@ -99,50 +98,5 @@ final class ToastView: UIView {
                 }
             }
         }
-    }
-}
-
-//SwiftUI ToastModifier
-struct ToastModifier: ViewModifier {
-    @Binding var isPresented: Bool
-    let message: String
-    let type: ToastType
-    let duration: TimeInterval
-
-    func body(content: Content) -> some View {
-        ZStack {
-            content
-            if isPresented {
-                VStack {
-                    HStack {
-                        Image(systemName: type == .success ? "checkmark.circle" : "xmark.octagon")
-                            .foregroundColor(.white)
-                        Text(message)
-                            .foregroundColor(.white)
-                            .bold()
-                    }
-                    .padding()
-                    .background(Color(type.backgroundColor))
-                    .cornerRadius(10)
-                    .padding(.top, 10)
-                    Spacer()
-                }
-                .transition(.move(edge: .top))
-                .animation(.easeInOut, value: isPresented)
-                .onAppear {
-                    DispatchQueue.main.asyncAfter(deadline: .now() + duration) {
-                        withAnimation {
-                            isPresented = false
-                        }
-                    }
-                }
-            }
-        }
-    }
-}
-
-extension View {
-    func toast(isPresented: Binding<Bool>, message: String, type: ToastType, duration: TimeInterval = 2.0) -> some View {
-        self.modifier(ToastModifier(isPresented: isPresented, message: message, type: type, duration: duration))
     }
 }
